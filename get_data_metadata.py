ca7d5d7f080bf599b52b3cd1d7423e596dc74077
Alex Hutko <ahutko@namazu.dhcp4.washington.edu>
2018-02-01 17:24:41 -0800

    added minimum Npts = 3 when diffing trace

--- get_data_metadata.py
+++ get_data_metadata.py
@@ -89,12 +89,12 @@ def raw_trace_to_ground_motion_filtered_pruned(TraceOrig,T1,T2,AccVelDisp,FullRe
                 GMnative = "VEL"
             if ( GMnative == GM ):
                 idonothing = 1
-            elif ( GMnative == "ACC" and GM == "VEL" ):
+            elif ( GMnative == "ACC" and GM == "VEL" and trace.stats.npts > 2 ):
                 trace.integrate(method='cumtrapz')
             elif ( GMnative == "ACC" and GM == "DISP" ):
                 trace.integrate(method='cumtrapz')
                 trace.integrate(method='cumtrapz')
-            elif ( GMnative == "VEL" and GM == "ACC" ):
+            elif ( GMnative == "VEL" and GM == "ACC" and trace.stats.npts > 2 ):
                 trace.differentiate(method='gradient')
         #----- remove the full response
         elif ( FullResponse == 1 ):
